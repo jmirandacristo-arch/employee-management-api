95c2906a148f853fd4d85aefbcc912c83c19fff7
mirande_devpy <jmirandacristo@gmail.com>
2026-04-25 21:21:28 -0500

    Delete the database table creation script

--- create_tables.py
+++
@@ -1,5 +0,0 @@
-from app.infrastructure.database.database import engine, Base
-from app.infrastructure.database.models import EmployeeModel
-
-Base.metadata.create_all(bind=engine)
-print("Tables created successfully")
